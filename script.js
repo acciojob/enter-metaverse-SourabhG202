5914fb19b438be1607ffb861e744759b5592fc2e
Sourabh G <154436409+SourabhG202@users.noreply.github.com>
2025-04-09 11:45:26 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,5 @@
 //your JS code here. If required.
+document.getElementById('enterBtn').addEventListner('click',function() {
+	document.getElementById('status').innerHTML="<h1>Entered Metaverse</h1>"
+	
+});
